b3f60a6a22b263909b0f210f305efebbfae73ce4
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-02-08 18:55:13 +0100

    Debug

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,6 @@ fn handle_client_connection(stream: &mut TcpStream) -> Result<()> {
     let bytes_read = stream.read_to_end(&mut buffer)?;
     buffer.truncate(bytes_read);
     dbg!(String::from_utf8_lossy(&buffer));
-    loop {
-    }
     for command in buffer.split(|&byte| byte == b'\n') {
         let mut command = String::from_utf8_lossy(&command);
         command.to_mut().make_ascii_lowercase();

08b32e5a2dc6a5ebcb81e135d7206a5028fc274d
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-02-08 18:59:44 +0100

    Read input in loop

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,18 @@ fn main() {
 }
 
 fn handle_client_connection(stream: &mut TcpStream) -> Result<()> {
-    let mut buffer: Vec<u8> = vec![0; 1024];
-    let bytes_read = stream.read_to_end(&mut buffer)?;
-    buffer.truncate(bytes_read);
-    dbg!(String::from_utf8_lossy(&buffer));
-    for command in buffer.split(|&byte| byte == b'\n') {
-        let mut command = String::from_utf8_lossy(&command);
-        command.to_mut().make_ascii_lowercase();
-        dbg!(&command);
-        if command.contains("ping") {
-            stream.write_all(b"+PONG\r\n")?;
+    loop {
+        let mut buffer: Vec<u8> = vec![0; 1024];
+        let bytes_read = stream.read(&mut buffer)?;
+        buffer.truncate(bytes_read);
+        dbg!(String::from_utf8_lossy(&buffer));
+        for command in buffer.split(|&byte| byte == b'\n') {
+            let mut command = String::from_utf8_lossy(&command);
+            command.to_mut().make_ascii_lowercase();
+            dbg!(&command);
+            if command.contains("ping") {
+                stream.write_all(b"+PONG\r\n")?;
+            }
         }
     }
-    Ok(())
 }

b59f9ff7ad75a95be92546dc6a0d1f988253e465
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-02-19 18:16:40 +0100

    Reapply "Handle command line argument parsing myself to remove clap"
    This reverts commit 3604bd04c8548100821a4fbedd51120a864f518c.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use redis_starter_rust::{ start_redis_server, Config };
 use std::net::SocketAddr;
 use std::str::FromStr;
-use clap::Parser;
+use std::path::PathBuf;
+use std::env;
+// use clap::Parser;
 
 
 fn main() {
-    let args = Config::parse();
-    dbg!(&args);
-    start_redis_server(SocketAddr::from_str("127.0.0.1:6379").expect("hard coded SocketAddr"), args);
+    let args: Vec<_> = env::args().collect();
+    let config = match (args[1].as_str(), args[3].as_str()) {
+        ("--dir", "--dbfilename") => Config {
+            dir: PathBuf::from(&args[2]),
+            dbfilename: PathBuf::from(&args[4]),
+        },
+        ("--dbfilename", "--dir") => Config {
+            dir: PathBuf::from(&args[4]),
+            dbfilename: PathBuf::from(&args[2]),
+        },
+        _ => panic!("Unkown arguments"),
+    };
+    dbg!(&config);
+    start_redis_server(SocketAddr::from_str("127.0.0.1:6379").expect("hard coded SocketAddr"), config);
 }

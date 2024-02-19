8e186c1b6500a97c8a327fed4b29febc7fafb957
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-02-19 23:50:35 +0100

    Debug

--- src/persistence.rs
+++ src/persistence.rs
@@ -12,7 +12,7 @@ pub fn load_rdb_file(rdb_file_path: &Path) -> Result<Database> {
     let mut bytes: Vec<u8> = vec![];
     dbg!(rdb_file_path);
     file.read_to_end(&mut bytes).unwrap();
-    for byte in bytes {
+    for &byte in bytes.iter() {
         println!("{:02X} ", byte);
     }
     parse_rdb(&bytes)
